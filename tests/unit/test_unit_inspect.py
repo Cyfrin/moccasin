a16d25a471031a80eae7cfceddaddcf0246b46c8
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2024-10-22 18:02:34 -0400

    feat: made imports work with inspect

--- tests/unit/test_unit_inspect.py
+++ tests/unit/test_unit_inspect.py
@@ -9,6 +9,8 @@ def test_inspect_counter(complex_project_config):
     expected_dir = {
         "set_number(uint256)": "0xd6d1ee14 (3604082196)",
         "increment()": "0xd09de08a (3500007562)",
+        "number()": "0x8381f58a (2206332298)",
+        "other_number()": "0x81593bc0 (2170108864)",
     }
     current_dir = Path.cwd()
     try:
@@ -16,4 +18,4 @@ def test_inspect_counter(complex_project_config):
         result = inspect_contract("Counter", "function_signatures", print_out=False)
     finally:
         os.chdir(current_dir)
-    assert result == expected_dir
+    assert expected_dir == result
