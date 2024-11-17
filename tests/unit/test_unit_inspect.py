37a4ecf206ba6c57955a04fbe19dbcf432928d71
Patrick Collins <54278053+PatrickAlphaC@users.noreply.github.com>
2024-11-17 15:29:24 -0500

    Fix/inspect not compiling (#158)
    * fix: inspect works with imports now
    
    * fix: lint

--- tests/unit/test_unit_inspect.py
+++ tests/unit/test_unit_inspect.py
@@ -2,10 +2,9 @@ import os
 from pathlib import Path
 
 from moccasin.commands.inspect import inspect_contract
-from tests.conftest import COMPLEX_PROJECT_PATH
 
 
-def test_inspect_counter(complex_project_config):
+def test_inspect_counter(complex_project_config, complex_temp_path):
     expected_dir = {
         "set_number(uint256)": "0xd6d1ee14 (3604082196)",
         "increment()": "0xd09de08a (3500007562)",
@@ -14,8 +13,20 @@ def test_inspect_counter(complex_project_config):
     }
     current_dir = Path.cwd()
     try:
-        os.chdir(current_dir.joinpath(COMPLEX_PROJECT_PATH))
+        os.chdir(current_dir.joinpath(complex_temp_path))
         result = inspect_contract("Counter", "function_signatures", print_out=False)
     finally:
         os.chdir(current_dir)
     assert result == expected_dir
+
+def test_inspect_layout_imports(installation_project_config, installation_temp_path):
+    current_dir = Path.cwd()
+    try:
+        os.chdir(current_dir.joinpath(installation_temp_path))
+        result = inspect_contract("MyToken", "storage_layout", print_out=False)
+    finally:
+        os.chdir(current_dir)
+    layout = result["storage_layout"]
+    # Spot check
+    assert layout["ow"]["owner"] == {'type': 'address', 'n_slots': 1, 'slot': 0}
+    assert layout["erc20"]["balanceOf"] == {'type': 'HashMap[address, uint256]', 'n_slots': 1, 'slot': 1}
