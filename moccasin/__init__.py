0f0b1a15f9568aa8fc069a2981a6842665ca7f45
Patrick Collins <54278053+PatrickAlphaC@users.noreply.github.com>
2024-11-24 13:17:29 -0500

    fix: make it so users can rerun the setup

--- moccasin/__init__.py
+++ moccasin/__init__.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 
 from moccasin import __main__
 
@@ -11,15 +12,18 @@ def version() -> str:
     return __main__.get_version()
 
 
-def setup_notebook():
-    from moccasin.config import initialize_global_config
+def setup_notebook(path: str | Path | None = None):
+    from pathlib import Path
+
     from moccasin._sys_path_and_config_setup import (
         _set_sys_path,
-        get_sys_paths_list,
         _setup_network_and_account_from_config_and_cli,
+        get_sys_paths_list,
     )
+    from moccasin.config import get_or_initialize_config
 
-    config = initialize_global_config()
+    normalized_path: Path = Path(path) if path is not None else Path.cwd()
+    config = get_or_initialize_config(normalized_path)
 
     # Set up the environment (add necessary paths to sys.path, etc.)
     _set_sys_path(get_sys_paths_list(config))
